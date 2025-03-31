00cc501e84e467c09df2be00106f5d83149260a0
henry <henrykuzma42@gmail.com>
2025-03-31 16:15:42 -0400

    added visual labels to filters

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -54,7 +54,7 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
         auto center = bounds.getCentre();
 
 
-        //Draw Rectangle level for dials
+        //Draw level for dials
         Path p;
 
         Rectangle<float> r;
@@ -62,6 +62,7 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
         r.setRight(center.getX() + 2);
         r.setTop(bounds.getY());
         r.setBottom(center.getY()/* - rswl->getTextHeight() * 1.5*/);
+
 
         p.addRoundedRectangle(r, 2.f);
 
@@ -89,7 +90,7 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
 
         auto strWidth = g.getCurrentFont().getStringWidth(text);
 
-        r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+        r.setSize(strWidth-4, rswl->getTextHeight() + 2);
         r.setCentre(center.getX(), center.getY() + rswl->getTextHeight()*2);
 
         g.setColour(Colours::black);
@@ -113,10 +114,10 @@ void RotarySliderWithLabels::paint(juce::Graphics& g) {
 
     auto sliderBounds = getSliderBounds();
 
-    g.setColour(Colours::red);
+    /*g.setColour(Colours::red);
     g.drawRect(getLocalBounds());
     g.setColour(Colours::yellow); 
-    g.drawRect(sliderBounds);
+    g.drawRect(sliderBounds);*/
 
     getLookAndFeel().drawRotarySlider(g, 
                                       sliderBounds.getX(), 
@@ -127,6 +128,47 @@ void RotarySliderWithLabels::paint(juce::Graphics& g) {
                                       startAng,
                                       endAng, 
                                       *this);
+
+    //Draw a number of labels around the dials
+
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * .05f;
+
+    g.setColour(Colour(0u, 172u, 1u));
+    g.setFont(getTextHeight()*.8);
+    
+    auto numChoices = labels.size();
+    for (int i = 0; i < numChoices; i++) {
+
+        auto label = labels[i];
+        auto pos = labels[i].pos;
+        jassert(0.f <= pos);
+        jassert(pos <= 1.f);
+
+        auto ang  = jmap(pos, 0.f, 1.f, startAng, endAng);
+
+
+        auto c = center.getPointOnCircumference(radius + getTextHeight() *.5, ang);
+
+        Rectangle<float> r;
+        auto str = labels[i].label;
+        r.setSize(g.getCurrentFont().getStringWidth(str)*.8, float(getTextHeight()));
+        r.setCentre(c);
+        r.setY(r.getY() + getTextHeight()*.6);
+        if (i == 0)
+            r.setX(r.getX() - sliderBounds.getWidth()*.6); 
+        else
+            r.setX(r.getX() + sliderBounds.getWidth() *.6);
+
+
+
+        g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+
+
+
+    }
+
+
 
 }
 
@@ -159,6 +201,7 @@ juce::String RotarySliderWithLabels::getDisplayString() const {
     auto New = choiceParam->getCurrentChoiceName();*/
 
 
+
     juce::String str;
     bool addK = false;
 
@@ -175,7 +218,7 @@ juce::String RotarySliderWithLabels::getDisplayString() const {
     }
     else {          //THIS STATEMENT SHOULD NOT TRIGGER BUT SEEMS TO TRIGGER OFF OF THE LOW CUT SLOPES
         float val = getValue();
-        str << 12 + val*12;
+        str << 12 + val*12.0f;
         NULL;
         //str = suffix;
         //jassertfalse;
@@ -283,6 +326,10 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     auto& HF = monoChain.get<ChainPositions::HF>();
     auto& highcut = monoChain.get<ChainPositions::HighCut>();
 
+    auto chainSettings = getChainSettings(audioProcessor.apvts);
+
+   
+
 
     //ATTEMPTING IDEA TO DRAW DOTS ON LINE
     //auto LFfreq = LF.coefficients->getRawCoefficients();
@@ -331,7 +378,7 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
         mags[i] = Decibels::gainToDecibels(mag);
     }
-
+    //FLAT LINE
     Path main;
     main.startNewSubPath(responseArea.getX(), responseArea.getCentreY());
     main.lineTo(responseArea.getRight(), responseArea.getCentreY());
@@ -350,6 +397,8 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
     responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
 
+
+    //CREATE PATH FOR RESPONSE CURVE
     for (size_t i = 1; i < mags.size(); i++) {
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
@@ -361,6 +410,150 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     g.setColour(Colours::white);
     g.strokePath(responseCurve, PathStrokeType(2.f));
 
+
+    //DRAW POINTS
+    auto LF_Freq = chainSettings.lfFreq;
+    auto LM_Freq = chainSettings.lmFreq;
+    auto M_Freq = chainSettings.mFreq;
+    auto HM_Freq = chainSettings.hmFreq;
+    auto HF_Freq = chainSettings.hfFreq;
+
+    auto LF_Gain = chainSettings.lfGainInDecibels;
+    auto LM_Gain = chainSettings.lmGainInDecibels;
+    auto M_Gain = chainSettings.mGainInDecibels;
+    auto HM_Gain = chainSettings.hmGainInDecibels;
+    auto HF_Gain = chainSettings.hfGainInDecibels;
+
+
+
+
+    Path LFREQ; 
+    Rectangle<float> r;
+    r.setSize(10,10);
+
+    auto midpoint = 90;
+    
+    //Convert Frequency to Pixels
+    auto x = (LF_Freq / 20000);  
+    x = 600 * x;
+    //Account for Skew
+    x = (200 * log10(x)) + 44;
+
+
+    r.setCentre(responseArea.getX() + x, responseArea.getCentreY() + map(int(mags[x])) - midpoint);
+
+    LFREQ.addRoundedRectangle(r, 2.f);
+    g.setColour(juce::Colours::red);
+    g.fillPath(LFREQ);
+
+    //DEBUGGING
+   /* juce::String str;
+
+    str << "HM: Y = " << y << "| X = " << x << "| bottom=" << responseArea.getBottom();*/
+
+
+
+
+    Path LMFREQ;
+    Rectangle<float> r2;
+    r2.setSize(10, 10); 
+
+    //Convert Frequency to Pixels
+    x = (LM_Freq / 20000);
+    x = 600 * x;
+    //Account for Skew
+    x = (200 * log10(x)) + 44;
+    
+    r2.setCentre(responseArea.getX() + x, responseArea.getCentreY() + map(int(mags[x])) - midpoint);
+
+    LMFREQ.addRoundedRectangle(r2, 2.f); 
+    g.setColour(juce::Colours::orange);  
+    g.fillPath(LMFREQ); 
+
+    Path MFREQ;
+    Rectangle<float> r3;
+    r3.setSize(10, 10);
+
+   
+    //Convert Frequency to Pixels
+    x = (M_Freq / 20000);
+    x = 600 * x;
+    //Account for Skew
+    x = (200 * log10(x)) + 44;
+
+    r2.setCentre(responseArea.getX() + x, responseArea.getCentreY() + map(int(mags[x])) - midpoint);
+
+    MFREQ.addRoundedRectangle(r2, 2.f);
+    g.setColour(juce::Colours::yellow);
+    g.fillPath(MFREQ);
+
+    Path HMFREQ;
+    Rectangle<float> r4;
+    r4.setSize(10, 10);
+
+    //Convert Frequency to Pixels
+    x = (HM_Freq / 20000);
+    x = 600 * x;
+    //Account for Skew
+    x = (200 * log10(x)) + 44;
+
+    r2.setCentre(responseArea.getX() + x, responseArea.getCentreY() + map(int(mags[x])) - midpoint);
+
+    HMFREQ.addRoundedRectangle(r2, 2.f);
+    g.setColour(juce::Colours::green);
+    g.fillPath(HMFREQ);
+
+    Path HFFREQ;
+    Rectangle<float> r5;
+    r4.setSize(10, 10);
+
+    //Convert Frequency to Pixels
+    x = (HF_Freq / 20000);
+    x = 600 * x;
+    //Account for Skew
+    x = (200 * log10(x)) + 44;
+
+    r2.setCentre(responseArea.getX() + x, responseArea.getCentreY() + map(int(mags[x])) - midpoint);
+
+    HFFREQ.addRoundedRectangle(r2, 2.f);
+    g.setColour(juce::Colours::blue);
+    g.fillPath(HFFREQ);
+
+    
+
+    //DEBUG RECT ===========================================================================================
+    Path textp;
+    Rectangle<float> LOWMID;
+
+    /*juce::String str; 
+
+    str << "HM: Y = " << y << "| X = " << x << "| bottom=" << responseArea.getBottom();*/
+
+
+    LOWMID.setLeft(responseArea.getX());
+    LOWMID.setRight(responseArea.getX() + 50);
+    LOWMID.setBottom(responseArea.getY());
+    LOWMID.setTop(responseArea.getY() + 30);
+
+    LOWMID.setSize(350, 20);
+    LOWMID.setCentre(responseArea.getX()+400, responseArea.getY()+150);
+
+    textp.addRoundedRectangle(LOWMID, 2.f);
+    /*g.setColour(Colours::aqua);
+    g.fillPath(textp)*/;
+
+
+    
+
+   
+    
+    /*g.setColour(Colours::white);
+    g.drawFittedText(str, LOWMID.toNearestInt(), juce::Justification::centred, 1);*/
+
+  
+
+
+     
    
     //==================================================================================================================
 
@@ -392,7 +585,44 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
 
     LF_Value("LFV", 0), LM_Value("LMV", 0), M_Value("MV", 0), HM_Value("HMV", 0), HF_Value("HMV", 0), LowCut_Value("LCV", 0), HighCut_Value("HCV", 0)
 
+
 {
+
+
+    //Initialize Labels
+    LF_FreqSlider.labels.add({ 0.f, "20Hz" });
+    LM_FreqSlider.labels.add({ 0.f, "20Hz" });
+    M_FreqSlider.labels.add({ 0.f, "20Hz" });
+    HM_FreqSlider.labels.add({ 0.f, "20Hz" });
+    HF_FreqSlider.labels.add({ 0.f, "20Hz" });
+
+    LF_FreqSlider.labels.add({ 1.f, "20kHz" });
+    LM_FreqSlider.labels.add({ 1.f, "20kHz" });
+    M_FreqSlider.labels.add({ 1.f, "20kHz" });
+    HM_FreqSlider.labels.add({ 1.f, "20kHz" });
+    HF_FreqSlider.labels.add({ 1.f, "20kHz" });
+
+
+    
+    HF_GainSlider.labels.add({ 0.f, "-24db" });
+    LM_GainSlider.labels.add({ 0.f, "-24db" });
+    M_GainSlider.labels.add({ 0.f, "-24db" });
+    LF_GainSlider.labels.add({ 0.f, "-24db" });
+    HM_GainSlider.labels.add({ 0.f, "-24db" });
+
+    LF_GainSlider.labels.add({ 1.f, "+24db" });
+    LM_GainSlider.labels.add({ 1.f, "+24db" });
+    M_GainSlider.labels.add({ 1.f, "+24db" });
+    HM_GainSlider.labels.add({ 1.f, "+24db" });
+    HF_GainSlider.labels.add({ 1.f, "+24db" });
+
+    lowCutFreqSlider.labels.add({ 0.f, "12" });
+    lowCutFreqSlider.labels.add({ 1.f, "24" });
+    highCutFreqSlider.labels.add({ 0.f, "12" });
+    highCutFreqSlider.labels.add({ 1.f, "24" });
+
+
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
 
@@ -420,9 +650,120 @@ void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
     using namespace juce; 
     // (Our component is opaque, so we must completely fill the background with a solid colour)
 
+    auto cornerRadius = 0.f;
+    auto thickness = 3.f;
+    auto TOPMOD = 11;
+    auto BOTMOD = 3;
+    auto XMOD = 2;
+
     //HENRY
     //Fill area in black
-    g.fillAll (Colours::black);
+    g.fillAll(Colours::black);
+
+    g.setColour(juce::Colours::red); 
+    auto bounds = LF_FreqSlider.getBounds();
+    auto qBounds = LF_QSlider.getBounds();
+    auto left = bounds.getX() + XMOD +2;
+    auto right = bounds.getX() + bounds.getWidth() - XMOD;
+
+    auto width = bounds.getWidth();
+    auto height = bounds.getHeight() * 3 + BOTMOD;
+
+    auto top = bounds.getY() - TOPMOD;
+    auto bottom = qBounds.getBottom() + BOTMOD;
+
+    g.drawRoundedRectangle(Rectangle<float>(left, top, width - (.05 * width)-2, height + (.06 * height)), cornerRadius, thickness);
+
+  /*  g.drawRoundedRectangle(LF_FreqSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(LF_GainSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(LF_QSlider.getBounds().toFloat(), cornerRadius, thickness);  */
+
+    //g.drawHorizontalLine(LF_FreqSlider.getBounds().getBottom(), 4.f, 1.f);
+
+    g.setColour(juce::Colours::orange);
+    bounds = LM_FreqSlider.getBounds();
+    qBounds = LM_QSlider.getBounds(); 
+    left = bounds.getX() + XMOD;
+    right = bounds.getX() + bounds.getWidth() - XMOD;
+
+    width = bounds.getWidth();
+    height = bounds.getHeight() * 3 + BOTMOD;
+
+    top = bounds.getY() - TOPMOD;
+    bottom = qBounds.getBottom() + BOTMOD;
+
+    g.drawRoundedRectangle(Rectangle<float>(left, top, width - (.05 * width), height + (.06 * height)), cornerRadius, thickness); 
+
+    juce::Colour(0, 0, 0);
+    g.setColour(juce::Colours::yellow);  
+    bounds = M_FreqSlider.getBounds();
+    qBounds = M_QSlider.getBounds();
+    left = bounds.getX() + XMOD; 
+    right = bounds.getX() + bounds.getWidth() - XMOD; 
+
+    width = bounds.getWidth();
+    height = bounds.getHeight() * 3 + BOTMOD;
+
+    top = bounds.getY() - TOPMOD;
+    bottom = qBounds.getBottom() + BOTMOD;
+
+    g.drawRoundedRectangle(Rectangle<float>(left, top, width - (.05 * width), height + (.06 * height)), cornerRadius, thickness);
+
+
+   /* g.drawRoundedRectangle(M_FreqSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(M_GainSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(M_QSlider.getBounds().toFloat(), cornerRadius, thickness);*/
+
+    g.setColour(juce::Colours::green);
+
+    bounds = HM_FreqSlider.getBounds();
+    qBounds = HM_QSlider.getBounds();
+    left = bounds.getX() + XMOD;
+    right = bounds.getX() + bounds.getWidth() - XMOD;
+
+    width = bounds.getWidth();
+    height = bounds.getHeight() * 3 + BOTMOD;
+
+    top = bounds.getY() - TOPMOD;
+    bottom = qBounds.getBottom() + BOTMOD;
+
+    g.drawRoundedRectangle(Rectangle<float>(left, top, width - (.05 * width), height + (.06 * height)), cornerRadius, thickness);
+
+
+    /*g.drawRoundedRectangle(HM_FreqSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(HM_GainSlider.getBounds().toFloat(), cornerRadius, thickness);
+    g.drawRoundedRectangle(HM_QSlider.getBounds().toFloat(), cornerRadius, thickness);*/
+
+    g.setColour(juce::Colours::blue);
+    bounds = HF_FreqSlider.getBounds(); 
+    qBounds = HF_QSlider.getBounds(); 
+    left = bounds.getX() + XMOD;
+    right = bounds.getX() + bounds.getWidth() - XMOD;
+
+    width = bounds.getWidth();
+    height = bounds.getHeight() * 3 + BOTMOD;
+
+    top = bounds.getY() - TOPMOD;
+    bottom = qBounds.getBottom() + BOTMOD;
+
+    g.drawRoundedRectangle(Rectangle<float>(left, top, width - (.05 * width) -2, height + (.06 * height)), cornerRadius, thickness);
+
+
+  /*  g.drawHorizontalLine(top,left,right);
+    g.drawHorizontalLine(bottom,left,right); */
+    //g.drawVerticalLine(left,top,bottom); 
+    //g.drawVerticalLine(right,top,bottom);  
+
+
+
+    //g.drawRoundedRectangle(HF_FreqSlider.getBounds().toFloat(), cornerRadius, thickness);
+    //g.drawRoundedRectangle(HF_GainSlider.getBounds().toFloat(), cornerRadius, thickness);
+    //g.drawRoundedRectangle(HF_QSlider.getBounds().toFloat(), cornerRadius, thickness);
+
+
+
+
+
     
 }
 
@@ -433,7 +774,9 @@ void SimpleEQAudioProcessorEditor::resized()
 
     //Reserve top third for Spectrograph
     auto bounds = getLocalBounds();
-    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.5);
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.45);
+
+    auto pad = bounds.removeFromTop(bounds.getHeight() * .05);
 
     responseCurveComponent.setBounds(responseArea);
 
@@ -449,32 +792,35 @@ void SimpleEQAudioProcessorEditor::resized()
     auto lfArea = bounds.removeFromLeft(bounds.getWidth() * .2);
 
     LF_FreqSlider.setBounds(lfArea.removeFromTop(lfArea.getHeight() * 0.33)); 
-    LF_GainSlider.setBounds(lfArea.removeFromTop(lfArea.getHeight() * 0.50)); 
-    LF_QSlider.setBounds(lfArea); 
+    LF_GainSlider.setBounds(lfArea.removeFromTop(lfArea.getHeight() * 0.475)); 
+    LF_QSlider.setBounds(lfArea.removeFromTop(lfArea.getHeight() * 0.9));
 
     auto lmArea = bounds.removeFromLeft(bounds.getWidth() * .25);
 
     LM_FreqSlider.setBounds(lmArea.removeFromTop(lmArea.getHeight() * 0.33));
-    LM_GainSlider.setBounds(lmArea.removeFromTop(lmArea.getHeight() * 0.50));
-    LM_QSlider.setBounds(lmArea);
+    LM_GainSlider.setBounds(lmArea.removeFromTop(lmArea.getHeight() * 0.475));
+    LM_QSlider.setBounds(lmArea.removeFromTop(lmArea.getHeight() * 0.9));
 
     auto mArea = bounds.removeFromLeft(bounds.getWidth() * .33);
 
     M_FreqSlider.setBounds(mArea.removeFromTop(mArea.getHeight() * 0.33));
-    M_GainSlider.setBounds(mArea.removeFromTop(mArea.getHeight() * 0.50));
-    M_QSlider.setBounds(mArea);
+    M_GainSlider.setBounds(mArea.removeFromTop(mArea.getHeight() * 0.475));
+    M_QSlider.setBounds(mArea.removeFromTop(mArea.getHeight() * 0.9));
+
+    //M_QSlider.setBounds(mArea);
 
     auto hmArea = bounds.removeFromLeft(bounds.getWidth() * .5);
 
     HM_FreqSlider.setBounds(hmArea.removeFromTop(hmArea.getHeight() * 0.33));
-    HM_GainSlider.setBounds(hmArea.removeFromTop(hmArea.getHeight() * 0.50));
-    HM_QSlider.setBounds(hmArea);
+    HM_GainSlider.setBounds(hmArea.removeFromTop(hmArea.getHeight() * 0.475));
+    HM_QSlider.setBounds(hmArea.removeFromTop(hmArea.getHeight()* 0.9));
 
     auto hfArea = bounds;
 
     HF_FreqSlider.setBounds(hfArea.removeFromTop(hfArea.getHeight() * 0.33));
-    HF_GainSlider.setBounds(hfArea.removeFromTop(hfArea.getHeight() * 0.50));
-    HF_QSlider.setBounds(hfArea);
+    HF_GainSlider.setBounds(hfArea.removeFromTop(hfArea.getHeight() * 0.475));
+    HF_QSlider.setBounds(hfArea.removeFromTop(hfArea.getHeight() * 0.9));
+
  
 }
 
